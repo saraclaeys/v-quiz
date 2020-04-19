09b7738b6611add8320a5e79161b0d1c78eb5141
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 19:37:06 +0200

    question?

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="question-box-container">
-    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-      <template v-slot:header>BootstrapVue</template>
-
-      <template v-slot:lead>
-        This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-        featured content or information.
-      </template>
+    <b-jumbotron>
+      <template v-slot:lead>Some question here?</template>
 
       <hr class="my-4" />
 
-      <p>
-        It uses utility classes for typography and spacing to space content out within the larger
-        container.
-      </p>
+      <p>List of answers</p>
 
-      <b-button variant="primary" href="#">Do Something</b-button>
-      <b-button variant="success" href="#">Do Something Else</b-button>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template
