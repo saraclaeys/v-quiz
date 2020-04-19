b8331a8bcee066beb589c77a017d89a0be56a7cb
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 19:32:08 +0200

    jumbotron

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Question Box</h1>
+  <div class="question-box-container">
+    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+      <p>For more information visit website</p>
+      <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
+  </div>
 </template
