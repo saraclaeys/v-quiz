83117bd5975e07f6f6aadfb9dba5a047e6e1a485
saraclaeys <sara.claeys01@gmail.com>
2020-04-19 19:27:23 +0200

    disable links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <b-nav>
-      <b-nav-item active>Fancy Quiz App</b-nav-item>
-      <b-nav-item>Counter: 4/10</b-nav-item>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Fancy Quiz App</b>
+      </b-nav-item>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item>
     </b-nav>
   </div>
 </template
