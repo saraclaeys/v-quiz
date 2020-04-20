ba39e48d9c3bfecbfb2510ca7c64de4d09f78389
saraclaeys <sara.claeys01@gmail.com>
2020-04-20 15:56:44 +0200

    questions opentdb

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,22 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: []
+    };
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
+      method: "get"
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
   }
 };
 </script>
